d3c162b2fc822337393935ad71efe2c786a0b217
yakibuta888 <yakibuta888@gmail.com>
2025-09-01 14:25:32 +0900

    fix: コンテナ名で文字起こし処理のコンテナとわかるようにする

--- script/transcriber.sh
+++ script/transcriber.sh
@@ -24,6 +24,7 @@ docker run --rm \
   --gpus all \
   --hostname transcriber \
   --add-host transcriber:127.0.0.1 \
+  --name app-transcriber_$(date +"%Y%m%d_%H%M%S") \
   -v "$USERDIR:/home/client/app/files" \
   transcriber \
   bash -c "sudo chmod -R 777 /home/client/app/files && python src/cui/main_cui.py --model $MODEL --infile ./files/$AUDIOFILE --outname \"$OUTNAME\" $DIARIZE_ARG && sudo cp /home/client/app/src/logs/transcriber.log /home/client/app/files"
